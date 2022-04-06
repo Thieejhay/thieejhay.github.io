2df9c1df2c1a4c07ea00f77ec0007b5c1278122f
Thieejhay <101231923+Thieejhay@users.noreply.github.com>
2022-04-06 19:47:06 +0100

    Add files via upload

---
+++ java2.js
@@ -0,0 +1,30 @@
+var age = prompt("What is your age");
+
+function checkDriverAge(age) {
+	console.log(Number(age));
+	if(Number(age) < 18) {
+		alert("Sorry, you are too young to drive this car. Powering off");
+	}
+	else if(Number(age) > 18) {
+		alert("Powering On. Enjoy the ride!");
+	}
+	else if(Number(age) === 18) {
+		alert("Congratulations on your first year of driving. Enjoy the ride!");
+	}
+}
+
+var checkDriverAge2 = function(age) {
+	console.log(Number(age));
+	if(Number(age) < 18) {
+		alert("Sorry, you are too young to drive this car. Powering off");
+	}
+	else if(Number(age) > 18) {
+		alert("Powering On. Enjoy the ride!");
+	}
+	else if(Number(age) === 18) {
+		alert("Congratulations on your first year of driving. Enjoy the ride!");
+	}
+}
+
+
+
